ec1c5258d49287597f138aaf20cb198a94bdf531
Ganesh-Kodali <codebind@LAPTOP-PB77785G.localdomain>
2025-04-28 12:03:54 +0000

    xor

---
+++ prac_19_4/xor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int xor_upto_n(int n) {
+    if (n % 4 == 0)
+        return n;
+    else if (n % 4 == 1)
+        return 1;
+    else if (n % 4 == 2)
+        return n + 1;
+    else 
+        return 0;
+}
+
+int main() {
+    int n = 10;
+    printf("XOR from 1 to %d is: %d\n", n, xor_upto_n(n));
+    return 0;
+}
+
