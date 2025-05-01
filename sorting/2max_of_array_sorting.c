473b75e908ce4c9e7c8f791def81e67b5f662ded
Ganesh-Kodali <codebind@LAPTOP-PB77785G.localdomain>
2025-05-01 12:05:48 +0000

    max n min of array using sorting

---
+++ sorting/2max_of_array_sorting.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int i,j,n,temp;
+printf("enter no of elements in the array\n");
+scanf("%d",&n);
+int a[n];
+printf("enter the array elements\n");
+for(i=0;i<n;i++){
+scanf("%d",&a[i]);
+}
+for(i=0;i<(n-1);i++)
+{
+        for(j=i+1;j<n;j++){
+                if(a[j]<a[i]){
+                        temp=a[j];
+                        a[j]=a[i];
+                        a[i]=temp;
+                }
+        }
+}
+printf("2ndmax of array is\n");
+
+printf("%d\n",a[n-2]);
+}
