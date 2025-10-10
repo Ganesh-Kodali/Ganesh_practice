95eef3e9f3cc846399a1c6585c5fa977fa50d8aa
Ganesh <ganeshkodali346@gmail.com>
2025-10-10 12:11:10 +0530

    cmake-practice

---
+++ cmake_practice/cmake1/build/CMakeFiles/Ganesh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ganesh.dir/src/hello.c.o"
+  "Ganesh"
+  "Ganesh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ganesh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake_practice/cmake2/text/app.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+#include"math.h"
+
+int main()
+{
+printf("adding 2 numbers %d\n",add(5,6));
+}
