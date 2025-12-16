5e2abecdf867681d1b46b4d775e86ccad2ea403d
Ganesh <ganeshkodali346@gmail.com>
2025-12-16 18:47:17 +0530

    docker

---
+++ Docker/docker/helloworld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World from my C file inside Docker!\n");
+    return 0;
+}
+
---
+++ Docker/docker1/app.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+#include"add.h"
+
+#include"sub.h"
+
+int main()
+{
+	int a=5,b=6;
+	printf("\naddition is %d\n sub is %d\n",add(a,b),sub(a,b));
+}
