ff3273de6311df1a21663785df1374529bdd25bc
Ganesh Kodali <ganeshkodali346@gmail.com>
2026-04-06 18:03:16 +0530

    practice

--- jira/sys_test/q7_binarysearch.c
+++ jira/sys_test/q7_binarysearch.c
@@ -5,9 +5,8 @@ int main()
 int a[]={8,10,15,20,23,25,36,40,48,50,52,59,61,66,68,71,74,78,81,83};
 
 int last=(sizeof(a)/sizeof(int))-1;
-printf("%d\n",last);
 int first=0;
-int mid,flag=0;
+int mid;
 int key;
 printf("enter the value to find\n");
 scanf("%d",&key);
@@ -16,9 +15,8 @@ while(first<=last)
 	mid=(last+first)/2;
 if(key==a[mid])
 {
-	flag=1;
 printf("the element is found  %d\n",mid+1);
-break;
+return 0;
 }
 else if(key<a[mid])
 {
@@ -29,8 +27,5 @@ else
 first=mid+1;
 }
 }
-if(!flag)
-{
 printf("the number is not found\n");
-}
 }
